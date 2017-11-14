ca23461582f68334e1523e0c00476976298765d8
ronyn0 <31914274+ronyn0@users.noreply.github.com>
2017-11-14 17:37:47 -0500

    Update notes14NOV.cpp

--- notes14NOV.cpp
+++ notes14NOV.cpp
@@ -34,3 +34,27 @@ void replaceRand(int &a, int &b)  [
     b = rand() % 6 + 1);
 
 }
+void coinToss() {
+    srand(time(NULL));
+    
+    cout << "If you do a thousand coin tosses you could see: " << endl;
+    int counter = 1, prev = rand() % 2;
+    int count6 = 0, count7 = 0, count8 = 0, count9 = 0;
+    for(int i = 0; i < 1000; i++)   {
+        int next = rand() % 2;
+        if(next == prev) {counter++;}
+        else {
+            switch(counter) {
+                case 6: count6++; break;
+                case 7: count7++; break;
+                case 8: count8++; break;
+                case 9: count9++; break;
+                default: break;
+            }
+            counter = 1;
+        }
+        prev = next;
+    }
+    cout << "6 in a row: " << count6 << "\n7 in a row: " << count7 
+        << "\n8 in a row: " << count8 << "\n9 in a row: " << count9 << endl;
+}
